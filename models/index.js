330186a4b65a521902b059525ff08000ae8c54c7
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-08-28 00:11:58 +0100

    feat: use team, player, sponsorship models

--- models/index.js
+++ models/index.js
@@ -1,18 +1,49 @@
+let models = {}
+
 async function setupModels({ sequelize, Sequelize }) {
-  const User = await sequelize.define('user', {
-    firstName: {
+  await sequelize.drop()
+
+  const Team = await sequelize.define('team', {
+    name: {
       type: Sequelize.STRING
     },
-    lastName: {
+    stadium: {
       type: Sequelize.STRING
     }
-  });
+  })
 
-  return {
-    User
+  const Player = await sequelize.define('player', {
+    name: {
+      type: Sequelize.STRING
+    },
+    position: {
+      type: Sequelize.ENUM('GK', 'DEF', 'MID', 'ATT')
+    }
+  })
+
+  const Sponsorship = await sequelize.define('sponsorship', {
+    company: {
+      type: Sequelize.STRING
+    },
+    value: {
+      type: Sequelize.INTEGER
+    }
+  })
+
+  Team.hasMany(Player, { foreignKey: { allowNull: false, onDelete: 'CASCADE' }})
+  Player.hasMany(Sponsorship, { foreignKey: { allowNull: false, onDelete: 'CASCADE' }})
+
+  models = {
+    Team,
+    Player,
+    Sponsorship
   }
+
+  await sequelize.sync({force: true})
+  return models
 }
 
 module.exports = {
+  getModels: () => models,
   setupModels
 }
