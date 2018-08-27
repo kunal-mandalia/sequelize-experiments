327296a1c8164fc1ec15d5763d0294ecd2bd4d76
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-08-28 00:15:41 +0100

    feat: add main queries following startup queries

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const { sequelize, Sequelize } = require('./db')
 const { setupModels } = require('./models/index')
-const { initializationQueries } = require('./queries/initialization')
+const { startupQueries } = require('./queries/startup')
+const { mainQueries } = require('./queries/main')
 
 async function main() {
   try {
     await sequelize.authenticate()
     // setup models
     const models = await setupModels({ sequelize, Sequelize })
-    // init query
-    await initializationQueries({ sequelize, models })
-    // 
+    // setup seed data
+    await startupQueries({ sequelize, models })
+    // query data
+    await mainQueries({ models })
   } catch (error) {
-    console.log("Main error: ", error)
+    console.log("main error: ", error)
   }
 }
 main()
